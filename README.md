d8bd66c640f7bccc5e69bcde4ff76aaa1ad63cbf
PanicP <filmous2136@gmail.com>
2020-12-08 11:42:20 +0700

    Done: Create react app from scratch

---
+++ README.md
@@ -0,0 +1,1 @@
+# panicp-profile
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>
+    <h1>Hello</h1>
+    <p>test</p>
+</div>
+
+ReactDOM.render(<App />, document.getElementById("root"))
