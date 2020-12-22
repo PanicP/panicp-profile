164a0cba0ab2eae7669ce8135f7e621f0006ba95
PanicP <filmous2136@gmail.com>
2020-12-22 22:46:29 +0700

    WIP: Remove unused file

--- src/components/chat/Component2.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export default (
-  <>
-    <div>com2</div>
-  </>
-)
--- src/components/chat/index.ts
+++
@@ -1,7 +0,0 @@
-import Com1 from './Component1'
-import Com2 from './Component2'
-
-export {
-  Com1,
-  Com2
-}
