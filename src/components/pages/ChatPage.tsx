18b8b534d3912f1f7291bb409f93906af772fea8
PanicP <filmous2136@gmail.com>
2021-01-03 02:34:19 +0700

    wip: add some sentence and some css

--- src/components/pages/ChatPage.tsx
+++ src/components/pages/ChatPage.tsx
@@ -7,14 +7,24 @@ export default () => {
     const mySentence = {
         1: 'Welcome to my simple portfolio.',
         2: 'Do you want to know something about me ?',
+        3: 'So, I have some side projects and my hobbies.',
+        4: 'Which one do you want to see ?'
     }
 
     const yourSentence = {
         1: 'Yes.',
-        2: 'No.'
+        2: 'No.',
+        3: 'Side projects',
+        4: 'Hobbies'
     }
 
+    // mines
     const greeting = [ mySentence[1], mySentence[2] ]
+    const askingEtc = [ mySentence[3], mySentence[4] ]
+
+    // yours
+    const ansGreeting = [ yourSentence[1], yourSentence[2] ]
+    const ansAskingEtc = [ mySentence[3], mySentence[4] ]
 
     return (
         <>
