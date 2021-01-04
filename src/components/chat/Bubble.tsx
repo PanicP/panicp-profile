45d8f7ebbb0bfa287c5dff449e9b015b381430d4
PanicP <filmous2136@gmail.com>
2021-01-04 21:35:16 +0700

    wip: add delay

--- src/components/chat/Bubble.tsx
+++ src/components/chat/Bubble.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Bubble.scss'
 
 interface IChatBubble {
@@ -11,11 +11,23 @@ const ChatBubble = ({ label }: IChatBubble) => {
     console.log('testonclick')
   }
 
+  const timeout = (delay: number) => {
+    return new Promise(res => setTimeout(res, delay))
+  }
+
+  const [isRender, setRender] = useState(false)
+
+  useEffect(() => {
+    setTimeout(function () {
+      setRender(true)
+    }, 1000)
+  }, [])
+
   return (
     <div>
-      <div className='bubble-container' onClick={ () => handleOnClickBubble()}>
+      <div className='bubble-container' onClick={() => handleOnClickBubble()}>
         <div className='bubble-label'>
-          {label}
+          { isRender ? label : '...'}
         </div>
       </div>
     </div>
