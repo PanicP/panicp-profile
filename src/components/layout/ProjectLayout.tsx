5d456c8861c0545362496e278684dba0088296ca
PanicP <filmous2136@gmail.com>
2021-02-08 16:56:33 +0700

    wip: add link to linkedin and github

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -7,26 +7,43 @@ interface DefaultLayout {
   children: React.ReactNode
   // any other props that come into the component
 }
-// 
+
+interface INavigateToExternalUrl {
+  url: string
+  shouldOpenNewTab: boolean
+}
+
+const navigateToExternalUrl = ({ url, shouldOpenNewTab }: INavigateToExternalUrl) => (shouldOpenNewTab ? window.open(url, '_blank') : (window.location.href = url))
+//
 const ProjectLayout = ({ children }: DefaultLayout) => (
   <div className="project-layout">
     <div className="project-body">
       <div className="empty-space" />
-      <div className="project">
-        {children}
-      </div>
+      <div className="project">{children}</div>
       <div className="empty-space">
-        <div>
-          <img src={GithubIcon} alt='github' width={48} height={48} />
-          <img src={LinkedinIcon} alt='linkedin' width={48} height={48} />
+        <div className="icons-container">
+          <img
+            className="icon"
+            onClick={() => navigateToExternalUrl({ url: 'https://github.com/PanicP', shouldOpenNewTab: true, })}
+            src={GithubIcon}
+            alt="github"
+            width={24}
+            height={24}
+          />
+          <img
+            className="icon"
+            onClick={() => navigateToExternalUrl({ url: 'https://www.linkedin.com/in/thunthian-pornmuneewong-04bbb5190/', shouldOpenNewTab: true, })}
+            src={LinkedinIcon}
+            alt="linkedin"
+            width={24}
+            height={24}
+          />
         </div>
       </div>
     </div>
-    
+
     <div className="project-footer">
-      <div>
-        Created By PanicP
-      </div>
+      <div>Created By PanicP</div>
     </div>
   </div>
 )
