41433d1c05e03ac4198a964ed59d9a7d143bd02f
PanicP <filmous2136@gmail.com>
2021-02-10 15:22:42 +0700

    wip: add back button on project page

--- src/components/project/ProjectContainer.tsx
+++ src/components/project/ProjectContainer.tsx
@@ -3,34 +3,34 @@ import ProjectBox from 'Components/project/ProjectBox'
 import './ProjectContainer.scss'
 
 const test2 = [
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  {title: 'test', label: 'test1'},
-  
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+  { title: 'test', label: 'test1' },
+
 ]
 
 const Projects = [
   {
-    image: '', 
-    title: 'To the skies', 
+    image: '',
+    title: 'To the skies',
     description: 'A 1v1 spaceship battle game with random enemy movement by Java.',
     src: 'https://github.com/PanicP/To-The-Skies',
     demo: ''
   },
   {
-    image: '', 
-    title: 'Spaaaaaaaam', 
+    image: '',
+    title: 'Spaaaaaaaam',
     description: 'A single column vsrg game with only 1 song (lol) by Pygame from Python.',
     src: 'https://github.com/PanicP/Spaaaaaaaam',
     demo: ''
   },
   {
-    image: '', 
-    title: 'Panic Profile', 
+    image: '',
+    title: 'Panic Profile',
     description: 'PanicP\'s Profile with a chatbot style by ReactJS',
     src: 'https://github.com/PanicP/Spaaaaaaaam',
     demo: ''
@@ -39,6 +39,11 @@ const Projects = [
 
 export default () => (
   <div className="project-container">
+    <div className="back-button">
+      <div>
+        &lt; Back to chat
+      </div>
+    </div>
     <div className="project-header">
       <div className="project-header-title">
         Projects
@@ -48,7 +53,7 @@ export default () => (
           </div>
     </div>
     <div className="project-list">
-      {Projects && Projects.map(data => <ProjectBox 
+      {Projects && Projects.map(data => <ProjectBox
         image={data.image}
         title={data.title}
         description={data.description}
