082276a856282ab67f9d228be7f2e6fc56ba4bc5
PanicP <filmous2136@gmail.com>
2021-02-21 21:02:51 +0700

    fix icon on chat layout

--- src/components/layout/ChatLayout.tsx
+++ src/components/layout/ChatLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
-// import GithubPNG from '../../static/pictures/github.png'
-// import LinkedPNG from '../../static/pictures/linkedin.png'
+import GithubIcon from 'Static/pictures/github.png'
+import LinkedinIcon from 'Static/pictures/linkedin.png'
+import PPIcon from 'Static/pictures/pplogo.svg'
 import './ChatLayout.scss'
 
 interface DefaultLayout {
@@ -9,16 +9,45 @@ interface DefaultLayout {
   // any other props that come into the component
 }
 
+interface INavigateToExternalUrl {
+  url: string
+  shouldOpenNewTab: boolean
+}
+
+const navigateToExternalUrl = ({ url, shouldOpenNewTab }: INavigateToExternalUrl) => (shouldOpenNewTab ? window.open(url, '_blank') : (window.location.href = url))
+
 const ChatLayout = ({ children }: DefaultLayout) => (
   <div className="chat-layout">
-    <div className="empty-space" />
+    <div className="empty-space">
+      <img
+        className="icon"
+        src={PPIcon}
+        alt="github"
+        width={96}
+        height={96}
+      />
+    </div>
     <div className="chat-container">
       {children}
     </div>
     <div className="empty-space">
-      <div>
-        <img src={'../../static/pictures/github.png'} />
-        <img src={'../../static/pictures/linkedin.png'} />
+      <div className="icons-container">
+        <img
+          className="icon"
+          onClick={() => navigateToExternalUrl({ url: 'https://github.com/PanicP', shouldOpenNewTab: true, })}
+          src={GithubIcon}
+          alt="github"
+          width={24}
+          height={24}
+        />
+        <img
+          className="icon"
+          onClick={() => navigateToExternalUrl({ url: 'https://www.linkedin.com/in/thunthian-pornmuneewong-04bbb5190/', shouldOpenNewTab: true, })}
+          src={LinkedinIcon}
+          alt="linkedin"
+          width={24}
+          height={24}
+        />
       </div>
     </div>
   </div>
